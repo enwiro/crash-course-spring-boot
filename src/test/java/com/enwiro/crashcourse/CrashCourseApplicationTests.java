b5982d9ff571609a0ec13b46420d3db630060cac
Enwiro <enwiro@live.fr>
2025-11-23 18:23:19 +0100

    feat(crash-course): Add Spring Boot Base

---
+++ src/test/java/com/enwiro/crashcourse/CrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enwiro.crashcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
